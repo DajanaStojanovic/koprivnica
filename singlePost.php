a8cca9c90c5a8612daacc4c13570b197788c54be
Dajana Stojanović <daja15@live.com>
2016-07-08 15:03:09 +0200

    sređen singlePost

--- singlePost.php
+++ singlePost.php
@@ -7,24 +7,30 @@ $singleEvent = $event->fetch(PDO::FETCH_OBJ);
 ?>
 <?php include_once "header.php"; ?>
 
-<img src="<?php echo $put;?>images/<?php echo $singleEvent->category;?>.jpg" alt="Slika kategorije" class="category_img" />
+<div class="single_post" style="text-align: center;">
+	<h1><?php echo $singleEvent->name;?></h1>
+</div>
 
-<div class="post_name"><h1><?php echo $singleEvent->name;?></h1></div>
 
-<div class="row content">
-	<div class="col-md-3">
+<div class="row single_post_content">
+
+
+	<div class="col-md-8 single_post_description">
+		<?php echo $singleEvent->description;?>
+	
+	<br/>
+	<div class="single_post_location">Lokacija: <strong><?php echo $singleEvent->location;?></strong></div>
+
+	<div class="single_post_day">Vrijeme: <strong><?php echo $singleEvent->start_day . "." . $singleEvent->start_month . "., " . $singleEvent->start_time . " - " . $singleEvent->finish_day . "." . $singleEvent->finish_month . "., " . $singleEvent->finish_time;?></strong></div>
+	
+</div>
+	<div class="col-md-4">
 	<?php if(isset($singleEvent->pic_extension)):?>
 		<img src="<?php echo $put;?>images/<?php echo $singleEvent->id;?>.<?php echo $singleEvent->pic_extension;?>" alt="Slika vijesti" class="img-responsive post_image" /> 
-	<?php endif;?>
-	</div>
+		<?php else:?>
+		<img src="images/placeholder.png" />
+		<?php endif;?>
 
-	<div class="col-md-9">
-		<?php echo $singleEvent->description;?>
 	</div>
-	<br/>
-	<?php echo $singleEvent->location;?>
-	<br/>
-	<?php echo $singleEvent->start_day . "." . $singleEvent->start_month . ". : " . $singleEvent->start_time . " - " . $singleEvent->finish_day . "." . $singleEvent->finish_month . ". : " . $singleEvent->finish_time;?>
-	
 </div>
 <?php include_once "footer.php"; ?>
